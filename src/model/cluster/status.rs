d586df9ec0b8fb40ab8002746686e05697b6283a
Stay1444 <quiquelluc@gmail.com>
2023-12-15 23:27:19 +0100

    Added comments

--- src/model/cluster/status.rs
+++ src/model/cluster/status.rs
@@ -5,15 +5,15 @@ use crate::model::PveType;
 #[derive(Serialize, Deserialize, Debug)]
 pub struct PveClusterStatus {
     pub id: String,
-    pub ip: Option<String>,
-    pub level: Option<String>,
-    pub local: Option<bool>,
+    pub ip: Option<String>,    // [node] IP of the resolved nodename.
+    pub level: Option<String>, // [node] Proxmox VE Subscription level, indicates if eligible for enterprise support as well as access to the stable Proxmox VE Enterprise Repository.
+    pub local: Option<bool>,   // [node] Indicates if this is the responding node.
     pub name: String,
-    pub nodeid: Option<i32>,
-    pub nodes: Option<i32>,
-    pub online: Option<bool>,
-    pub quorate: Option<bool>,
+    pub nodeid: Option<i32>, // [node] ID of the node from the corosync configuration.
+    pub nodes: Option<i32>,  // [cluster] Nodes count, including offline nodes.
+    pub online: Option<bool>, // [node] Indicates if the node is online or offline.
+    pub quorate: Option<bool>, // [cluster] Indicates if there is a majority of nodes online to make decisions
     #[serde(rename = "type")]
-    pub pve_type: PveType,
-    pub version: Option<i32>,
+    pub pve_type: PveType, // Indicates the type, either cluster or node. The type defines the object properties e.g. quorate available for type cluster.
+    pub version: Option<i32>, // [cluster] Current version of the corosync configuration file.
 }
