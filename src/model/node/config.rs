bfe04b56a33a59c1144c25de5084a5aa59a30e40
Stay1444 <quiquelluc@gmail.com>
2023-12-16 22:26:05 +0100

    Add missing field descriptions

--- src/model/node/config.rs
+++ src/model/node/config.rs
@@ -17,33 +17,29 @@ pub enum Property {
 #[derive(Deserialize, Debug)]
 pub struct NodeConfiguration {
     #[serde(default, deserialize_with = "crate::deserializers::non_empty_str")]
-    pub acme: Option<String>,
+    pub acme: Option<String>, // Node specific ACME settings.
     #[serde(default, deserialize_with = "crate::deserializers::non_empty_str")]
-    pub acmedomain0: Option<String>,
+    pub acmedomain0: Option<String>, // ACME domain and validation plugin
     #[serde(default, deserialize_with = "crate::deserializers::non_empty_str")]
-    pub acmedomain1: Option<String>,
+    pub acmedomain1: Option<String>, // ACME domain and validation plugin
     #[serde(default, deserialize_with = "crate::deserializers::non_empty_str")]
-    pub acmedomain2: Option<String>,
+    pub acmedomain2: Option<String>, // ACME domain and validation plugin
     #[serde(default, deserialize_with = "crate::deserializers::non_empty_str")]
-    pub acmedomain3: Option<String>,
+    pub acmedomain3: Option<String>, // ACME domain and validation plugin
     #[serde(default, deserialize_with = "crate::deserializers::non_empty_str")]
-    pub acmedomain4: Option<String>,
+    pub acmedomain4: Option<String>, // ACME domain and validation plugin
     #[serde(default, deserialize_with = "crate::deserializers::non_empty_str")]
-    pub acmedomain5: Option<String>,
+    pub acmedomain5: Option<String>, // ACME domain and validation plugin
     #[serde(default, deserialize_with = "crate::deserializers::non_empty_str")]
-    pub description: Option<String>,
+    pub description: Option<String>, // Description for the Node. Shown in the web-interface node notes panel. This is saved as comment inside the configuration file.
     #[serde(default, deserialize_with = "crate::deserializers::non_empty_str")]
-    pub digest: Option<String>,
-    #[serde(
-        default,
-        rename = "startall-onboot-delay",
-        deserialize_with = "crate::deserializers::non_empty_str"
-    )]
-    pub start_all_on_boot_delay: Option<String>,
+    pub digest: Option<String>, // Prevent changes if current configuration file has different SHA1 digest. This can be used to prevent concurrent modifications.
+    #[serde(default, rename = "startall-onboot-delay")]
+    pub start_all_on_boot_delay: Option<u64>, // Initial delay in seconds, before starting all the Virtual Guests with on-boot enabled.
     #[serde(
         default,
         rename = "wakeonlan",
         deserialize_with = "crate::deserializers::non_empty_str"
     )]
-    pub wake_on_lan: Option<String>,
+    pub wake_on_lan: Option<String>, // MAC address for wake on LAN
 }
--- src/model/node/tasks.rs
+++ src/model/node/tasks.rs
@@ -3,28 +3,28 @@ use serde::{Deserialize, Serialize};
 #[derive(Serialize, Debug)]
 pub struct GetTasksFilter {
     #[serde(rename = "errors")]
-    pub only_errors: Option<bool>,
-    pub limit: Option<u64>,
-    pub since: Option<u64>,
-    pub source: Option<TaskSource>,
-    pub start: Option<u64>,
+    pub only_errors: Option<bool>, // Only list tasks with a status of ERROR.
+    pub limit: Option<u64>,         // Only list this amount of tasks.
+    pub since: Option<u64>,         // Only list tasks since this UNIX epoch.
+    pub source: Option<TaskSource>, // List archived, active or all tasks.
+    pub start: Option<u64>,         // List tasks beginning from this offset.
     #[serde(
         rename = "statusfilter",
         deserialize_with = "crate::deserializers::non_empty_str"
     )]
-    pub status_filter: Option<String>,
+    pub status_filter: Option<String>, // List of Task States that should be returned.
     #[serde(
         rename = "typefilter",
         deserialize_with = "crate::deserializers::non_empty_str"
     )]
-    pub type_filter: Option<String>,
-    pub until: Option<u64>,
+    pub type_filter: Option<String>, // Only list tasks of this type (e.g., vzstart, vzdump).
+    pub until: Option<u64>,         // Only list tasks until this UNIX epoch.
     #[serde(
         rename = "userfilter",
         deserialize_with = "crate::deserializers::non_empty_str"
     )]
-    pub user_filter: Option<String>,
-    pub vmid: Option<u64>,
+    pub user_filter: Option<String>, // Only list tasks from this user.
+    pub vmid: Option<u64>,          // Only list tasks for this VM.
 }
 
 impl Default for GetTasksFilter {
